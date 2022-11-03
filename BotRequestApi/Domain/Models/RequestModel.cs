e93795144b794affd3e7e0b6cdfc90e11f7315d9
unknown <ozabello21@gmail.com>
2022-11-03 15:37:46 +0300

    Created layers and two model

---
+++ BotRequestApi/Domain/Models/RequestModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class RequestModel
+    {
+        [LoadColumn(1), ColumnName("Message")]
+        public string Message { get; set; }
+
+        [LoadColumn(2), ColumnName("Response")]
+        public string Response { get; set; }
+    }
+}
---
+++ BotRequestApi/Domain/Models/ResponseModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class ResponseModel
+    {
+
+        [ColumnName("PredictedLabel")]
+        public string Message;
+
+        [ColumnName("PredictedResponse")]
+        public string Response;
+
+        [ColumnName("Score")]
+        public float[] Score;
+    }
+}
